25a55d8334df24b3e44a8bfc43b98e8da77ec010
Lukas Jarosch <lukas.jarosch@mail.com>
2018-11-04 19:49:15 +0100

    basic logging setup using logrus
    Signed-off-by: Lukas Jarosch <lukas.jarosch@mail.com>

--- log.go
+++ log.go
@@ -1,1 +1,69 @@
 package main
+
+import (
+	"context"
+	"time"
+
+	"github.com/lukasjarosch/service-boilerplate/config"
+	"github.com/micro/go-micro/metadata"
+	"github.com/micro/go-micro/server"
+	"github.com/sirupsen/logrus"
+)
+
+// initLogging initializes a logrus Logger
+func initLogging(cfg config.LogConfiguration) *logrus.Logger {
+
+	l := logrus.New()
+	var formatter logrus.Formatter
+	if cfg.Format == "json" {
+		formatter = &logrus.JSONFormatter{
+			TimestampFormat: time.RFC3339,
+		}
+	} else {
+		formatter = &logrus.TextFormatter{}
+	}
+	l.SetFormatter(formatter)
+
+	logLevel, err := logrus.ParseLevel(cfg.Level)
+	if err != nil {
+		logLevel = logrus.InfoLevel
+	}
+	l.SetLevel(logLevel)
+	return l
+}
+
+// logWithContext attaches the function name, request-id and trace-id  to the baseLogger and returns a *logrus.Entry
+func logWithContext(ctx context.Context, functionName string) *logrus.Entry {
+	log := baseLogger.WithField("method", functionName)
+
+	if requestId := getValueFromMetadata(ctx, "x-request-id"); requestId != "" {
+		log = log.WithField("request_id", requestId)
+	}
+
+	if traceId := getValueFromMetadata(ctx, "x-b3-trace-id"); traceId != "" {
+		log = log.WithField("trace_id", traceId)
+	}
+
+	return log
+}
+
+// getValueFromMetadata extracts a value of a given key from the context's metadata.
+// If the key does not exist and empty string is returned
+func getValueFromMetadata(ctx context.Context, key string) string {
+	if md, ok := metadata.FromContext(ctx); ok {
+		if id, ok := md[key]; ok {
+			return id
+		}
+	}
+	return ""
+}
+
+// LogWrapper is the handler wrapper
+func LogWrapper(fn server.HandlerFunc) server.HandlerFunc {
+	return func(ctx context.Context, req server.Request, rsp interface{}) error {
+		l := logWithContext(ctx, req.Method())
+		l.Println(req.ContentType())
+		return fn(ctx, req, rsp)
+	}
+}
+
--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"log"
 	"time"
 
 	"github.com/micro/go-micro"
 	goConf "github.com/micro/go-config"
+	"github.com/sirupsen/logrus"
 
 	"github.com/lukasjarosch/service-boilerplate/proto/example"
 	"github.com/lukasjarosch/service-boilerplate/handler"
@@ -17,11 +17,13 @@ const ServiceName = "go.example.srv"
 
 var (
 	cfg config.ServiceConfiguration
+	baseLogger *logrus.Logger
 )
 
 // init is called on package initialization and can therefore be used to initialize global stuff like logging, config, ..
 func init() {
 	goConf.Scan(&cfg)
+	baseLogger = initLogging(cfg.Log)
 }
 
 func main() {
@@ -29,15 +31,20 @@ func main() {
 		micro.Name(ServiceName),
 		micro.RegisterTTL(time.Second*60),
 		micro.RegisterInterval(time.Second*15),
+		micro.WrapHandler(LogWrapper),
 	)
 	service.Init()
 
 	example.RegisterExampleHandler(service.Server(), new(handler.Example))
 
+	// attach instance to all consecutive logs by default
+	logger := baseLogger.WithField("instance", ServiceName + "." + service.Options().Server.Options().Id)
+	logger.Info("Starting service " + ServiceName)
+
 	// init database
 
 	// fire
 	if err := service.Run(); err != nil {
-		log.Fatal(err)
+		logger.Fatal(err)
 	}
 }
