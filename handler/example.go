2dd3064ba62739cb9dbac8d97293642f4a102086
Lukas Jarosch <lukas.jarosch@mail.com>
2018-12-10 20:27:34 +0100

    dunno what changed :D
    Signed-off-by: Lukas Jarosch <lukas.jarosch@mail.com>

--- handler/example.go
+++ handler/example.go
@@ -4,11 +4,11 @@ import (
 	"context"
 	"github.com/sirupsen/logrus"
 
-	ex "github.com/lukasjarosch/go-micro-svc-boilerplate/proto/example"
 	"github.com/lukasjarosch/go-micro-svc-boilerplate/datastore"
+	ex "github.com/lukasjarosch/go-micro-svc-boilerplate/proto/example"
 )
 
-type Example struct{
+type Example struct {
 	log *logrus.Logger
 }
 
@@ -19,17 +19,17 @@ func InitHelloHandler(log *logrus.Logger) *Example {
 func (e *Example) Hello(ctx context.Context, req *ex.HelloRequest, rsp *ex.HelloResponse) error {
 
 	user := &datastore.User{
-		Name: "Hans Peter",
+		Name:  "Hans Peter",
 		Email: "hans3@peter.com",
 	}
 
-	 err := datastore.CreateUser(user)
-	 if err != nil {
+	err := datastore.CreateUser(user)
+	if err != nil {
 		e.log.WithField("email", user.Email).WithError(err).Info("unable to create user")
-	 	return err
-	 }
+		return err
+	}
 
-	 e.log.WithField("user_id", user.ID).Info("created new user")
+	e.log.WithField("user_id", user.ID).Info("created new user")
 
 	return nil
 }
